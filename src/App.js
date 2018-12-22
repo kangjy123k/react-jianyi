1b662b40398676b1001f5477dae9e9932df62b28
kangjy123k <kjy092620@gmail.com>
2018-12-22 15:11:53 +0100

    change state with handlerevent

--- src/App.js
+++ src/App.js
@@ -10,13 +10,25 @@ class App extends Component {
       {name:'Stephanie',age: 26}
     ]
     
+     
+  }
+  switchNameHandler = () =>{
+   // console.log('Was clicked!');
+   // this.state.persons[0].name="kris wu";
+   this.setState({
+    persons: [
+      {name:'Jianyi',age: 28},
+      {name:'Manu',age: 29},
+      {name:'Stephanie',age: 21}
+    ]
+   })
   }
   render() {
     return (
       <div className="App">
        <h1>Hi,I'm a React App</h1>
        <p>This is really working!</p>
-       <button>Switch Name!</button>
+       <button onClick={this.switchNameHandler}>Switch Name!</button>
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: basket</Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
