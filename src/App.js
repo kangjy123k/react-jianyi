d2cdac9a0dfd7952f4ccad49cac7b15721184c4b
kangjy123k <kjy092620@gmail.com>
2018-12-22 14:39:19 +0100

    add state property

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'mAX',age: 28},
+      {name:'Manu',age: 29},
+      {name:'Stephanie',age: 26}
+    ]
+    
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Hi,I'm a React App</h1>
+       <p>This is really working!</p>
+       <button>Switch Name!</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: basket</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
-    );
+      );
+    //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi,I m react!')
+    //);
   }
 }
 
